1ae48410d436074bad2f5c8108b23d8bbab1165d
Jakub Mezulanik <mezuja@gmail.com>
2022-07-10 22:57:54 +0200

    swtichType buttons + logic

--- src/Screens/TimerScreen/TimerScreen.js
+++ src/Screens/TimerScreen/TimerScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useState } from 'react';
 
 import styles from './TimerScreen.style';
 
 function TimerScreen() {
+    const [switchType, setSwitchType] = useState("");
+
+    const switchTypeHandler = (type) => {
+        setSwitchType(type);
+    }
+
     return (
         <View>
             <Text>Timer Screen</Text>
+            <Text>{switchType}</Text>
+            <View>
+                <Button
+                    title="Manual"
+                    onPress={() => switchTypeHandler("Manual")}
+                    disabled={switchType === "Manual" && switchType !== ""}
+                />
+                <Button
+                    title="Automatic"
+                    onPress={() => switchTypeHandler("Automatic")}
+                    disabled={switchType === "Automatic" && switchType !== ""}
+                />
+            </View>
         </View>
     )
 }
