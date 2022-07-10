ac19db2b48a749e46ab562508a8b3740dbaee7a7
Jakub Mezulanik <mezuja@gmail.com>
2022-07-10 16:48:37 +0200

    native stack navigator installed

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+function ProjectHealthy() {
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+
+            </NavigationContainer>
+        </>
+    )
+}
