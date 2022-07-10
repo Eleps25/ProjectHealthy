52ff32fcaf7d460a3d172bc28d168347bf257cd6
Jakub Mezulanik <mezuja@gmail.com>
2022-07-10 17:13:51 +0200

    Navigation added + reorganization

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -1,13 +1,20 @@
 import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './Timer.style';
 
 function Timer() {
+    const navigation = useNavigation();
+    const onPressHandler = () => {
+        navigation.navigate('TimerScreen');
+    }
+
     return (
         <View style={styles.rootContainer}>
             <Text>TIMER</Text>
             <Button
                 title='Set Timer'
+                onPress={onPressHandler}
             />
         </View>
     )
