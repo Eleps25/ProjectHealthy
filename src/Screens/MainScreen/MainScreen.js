70105fdafdd6498f1f775d13ac8ee51ddcf01363
Jakub Mezulanik <mezuja@gmail.com>
2022-07-10 14:42:00 +0200

    Main screen - basic structure

---
+++ src/Screens/MainScreen/MainScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function MainScreen() {
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+export default MainScreen;
