0fae4fa1c1e61b073d34f939dbb797c2ec3593ca
Jakub Mezulanik <mezuja@gmail.com>
2022-07-11 17:22:07 +0200

    timer validation

--- src/Components/TimerSettings/TimerSettings.js
+++ src/Components/TimerSettings/TimerSettings.js
@@ -1,17 +1,58 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 import { useState } from 'react';
 
 function TimerSettings() {
     const [switchType, setSwitchType] = useState("");
+    const [time, setTime] = useState("");
+    const [repeat, setRepeat] = useState("");
+    const [isTimerActive, setIsTimerActive] = useState(false);
 
     const switchTypeHandler = (type) => {
         setSwitchType(type);
     }
 
+    const changeTimeHandler = (inputNumber) => {
+        setTime(inputNumber)
+    }
+
+    const changeRepeatHandler = (inputNumber) => {
+        setRepeat(inputNumber)
+    }
+
+    const startTimer = () => {
+        if (time == "") {
+            alert("Please add number to time");
+        } else if (parseInt(time) < 0) {
+            alert("Please add number higher or equal to 0 to time");
+        } else if (repeat == "") {
+            alert("Please add number to repeat times");
+        } else if (parseInt(repeat) < 0) {
+            alert("Please add number higher or equal to 0 to repeat times");
+        } else if (switchType === "") {
+            alert("Please choose switch type");
+        } else {
+            setIsTimerActive(true);
+        }
+    }
+
     return (
         <View>
-            <Text>Timer Screen</Text>
-            <Text>{switchType}</Text>
+            <View>
+                <Text>Time(s)</Text>
+                <TextInput
+                    keyboardType='numeric'
+                    value={time}
+                    onChangeText={changeTimeHandler}
+                />
+            </View>
+            <View>
+                <Text>Numbers of repeat</Text>
+                <TextInput
+                    keyboardType='numeric'
+                    value={repeat}
+                    onChangeText={changeRepeatHandler}
+                />
+            </View>
             <View>
                 <Button
                     title="Manual"
@@ -24,6 +65,11 @@ function TimerSettings() {
                     disabled={switchType === "Automatic" && switchType !== ""}
                 />
             </View>
+            <Button
+                title="Start Timer"
+                onPress={startTimer}
+            />
+            <Text>{isTimerActive ? "true" : "false"}</Text>
         </View>
     )
 }
