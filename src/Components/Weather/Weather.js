676af27972756f70826618d1cddbadb11c92a190
Jakub Mezulanik <mezuja@gmail.com>
2022-07-13 08:37:25 +0200

    Weather component initialization

---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+import styles from "./Weather.style";
+
+const Weather = () => {
+    return (
+        <View>
+            <Text>Weather</Text>
+        </View>
+    )
+}
+
+export default Weather;
--- src/Screens/MainScreen/MainScreen.js
+++ src/Screens/MainScreen/MainScreen.js
@@ -3,13 +3,13 @@ import { View, Text } from 'react-native';
 import styles from './MainScreen.style';
 
 import RandomQuote from '../../Components/RandomQuote/index.js';
-
+import Weather from '../../Components/Weather';
 import Timer from '../../Components/Timer';
 
 const MainScreen = () => {
     return (
         <View>
-            <Text>Test</Text>
+            <Weather />
             <View style={styles.qoutesContainer}>
                 <RandomQuote />
             </View>
